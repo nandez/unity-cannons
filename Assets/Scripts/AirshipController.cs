9ba8566c589975d5a8f912a7b116d8e3621b7ce0
German Fernandez <gfernandez@evosoftuy.com>
2022-12-13 19:34:46 -0300

    Updates for movement & environment

--- Assets/Scripts/AirshipController.cs
+++ Assets/Scripts/AirshipController.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class AirshipController : MonoBehaviour
 {
-    public float speed = 3f;
-    public Transform[] waypoints;
+    [SerializeField] protected LayerMask navigationLayer;
+    [SerializeField] protected float moveSpeed;
+    [SerializeField] protected float rotationSpeed;
+    [SerializeField] protected float movementMinDistance = 0.25f;
 
+    [SerializeField] protected Transform helix;
+    [SerializeField] protected float helixRotationSpeed = 100f;
 
-    public Transform helix;
-    public float helixRotationSpeed = 50f;
+    protected Vector3 target;
+    protected bool shouldMove = false;
 
-    private int currentWaypointIdx = 0;
+    //public Transform[] waypoints;
+    //private int currentWaypointIdx = 0;
 
     void Start()
     {
- 
+
     }
 
     void Update()
@@ -23,22 +28,46 @@ public class AirshipController : MonoBehaviour
         // Rotates the helix on x axis
         helix.Rotate(Vector3.right * helixRotationSpeed * Time.deltaTime);
 
-        // Handles waypoint movement..
-        if (currentWaypointIdx < waypoints.Length)
+        // Proyectamos un rayo desde la cámara hasta el punto donde se hizo click
+        // y verificamos la colisión con la capa de navegación..
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f, navigationLayer))
         {
-            var waypoint = waypoints[currentWaypointIdx];
-            transform.position = Vector3.MoveTowards(transform.position, waypoint.position, Time.deltaTime * speed);
-
-            var targetRotation = Quaternion.LookRotation(waypoint.position - transform.position, Vector3.up);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime);
-
-
-            if (Vector3.Distance(transform.position, waypoint.position) < 0.5f)
+            // Si el jugador presiona el click izquierdo, guardamos la posición para
+            // movernos.
+            if (Input.GetMouseButtonDown(0))
             {
-                currentWaypointIdx++;
+                target = hitInfo.point;
+                shouldMove = true;
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                // TODO: implementar el ataque..
+            }
+        }
 
-                if (currentWaypointIdx >= waypoints.Length)
-                    currentWaypointIdx = 0;
+        // Mantenemos la misma altura en todo momento..
+        target.y = transform.position.y;
+
+        if (shouldMove)
+        {
+            // Calculamos la dirección y la distancia.
+            var direction = target - transform.position;
+            var distance = direction.magnitude;
+
+            // Si la distancia es mayor al mínimo preestablecido,
+            // giramos y nos movemos hacia el punto objetivo.
+            if (distance > movementMinDistance)
+            {
+                var rotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            }
+            else
+            {
+                // En este caso, ya estamos cerca del punto objetivo, por lo que
+                // cancelamos el movimiento
+                shouldMove = false;
             }
         }
     }
