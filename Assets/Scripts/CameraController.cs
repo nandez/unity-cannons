c4d1bf5d6e55d32bff7716cbea6b320334e23cf5
German Fernandez <gfernandez@evosoftuy.com>
2022-12-13 01:16:59 -0300

    Added camera controller for panning & zoom

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] protected float moveSpeed = 10f;
+    [SerializeField] protected float borderThickness = 10f;
+    [SerializeField] protected float zoomSpeed = 20f;
+    [SerializeField] protected float maxHeight = 30f;
+    [SerializeField] protected float minHeight = 10f;
+    [SerializeField] protected Vector2 mapSize;
+
+    void Update()
+    {
+        var position = transform.position;
+
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - borderThickness)
+            position.z += moveSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= borderThickness)
+            position.z -= moveSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - borderThickness)
+            position.x += moveSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= borderThickness)
+            position.x -= moveSpeed * Time.deltaTime;
+
+        var sccroll = Input.GetAxis("Mouse ScrollWheel");
+        position.y -= sccroll * zoomSpeed * 100f * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, -mapSize.x, mapSize.x);
+        position.y = Mathf.Clamp(position.y, minHeight, maxHeight);
+        position.z = Mathf.Clamp(position.z, -mapSize.y, mapSize.y);
+
+        transform.position = position;
+    }
+}
